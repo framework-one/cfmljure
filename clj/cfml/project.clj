cc8a73113301d775074c533c2edb4e202c30cc47
Sean Corfield <sean@corfield.org>
2010-09-26 18:50:01 -0700

    Update to 1.3.0 SNAPSHOT builds for Clojure / Contrib.

--- clj/cfml/project.clj
+++ clj/cfml/project.clj
@@ -1,4 +1,5 @@
 (defproject cfml "1.0.0-SNAPSHOT"
   :description "cfmljure example project"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                   [org.clojure/clojure-contrib "1.2.0"]])
+  :dependencies
+    [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
+      [org.clojure.contrib/complete "1.3.0-SNAPSHOT" :classifier "bin"]])

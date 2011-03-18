2cd082b78acbeeabc64365860954b896758cdafc
Sean Corfield <sean@getrailo.com>
2011-03-18 11:03:50 -0700

    Revert to Clojure 1.2.0 to get things running again :)

--- clj/cfml/project.clj
+++ clj/cfml/project.clj
@@ -1,5 +1,5 @@
 (defproject cfml "1.0.0-SNAPSHOT"
   :description "cfmljure example project"
   :dependencies
-    [[org.clojure/clojure "1.3.0-master-SNAPSHOT"]
-      [org.clojure.contrib/complete "1.3.0-SNAPSHOT" :classifier "bin"]])
+    [[org.clojure/clojure "1.2.0"]
+     [org.clojure/clojure-contrib "1.2.0"]])
